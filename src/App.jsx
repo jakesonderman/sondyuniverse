a7c4d76e232a6ca88c2c1a48c2abbd604caadd42
jakesondy <87723705+jakesonderman@users.noreply.github.com>
2026-06-16 20:10:49 -0700

    react hash router import

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import {HashRouter as Router, Routes, Route} from "react-dom"
+import HomePage from './pages/HomePage.jsx'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={HomePage}/>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import "./HomePage.css"
+
+
+function HomePage(props) {
+    return(
+        <>
+            <h1>HEYY</h1>
+        </>
+    )
+}
+
+export default HomePage; 
